42bb1a970ec70b46291172aaff2a111b084d4a73
perumalprabhup <ppprabhu2010@gmail.com>
2017-12-22 10:31:39 +0530

    Organic Bytes First Commit

---
+++ src/assets/js/carousel.js
@@ -0,0 +1,9 @@
+   $(document).ready(function() {
+              $('.owl-carousel').owlCarousel({
+                margin: 10,
+                loop: true,
+                autoWidth: true,
+				autoplay:true,
+                items: 4
+              })
+            });
