b0fa9ac92a1ee4df591dff5b1888d6fe599eed93
perumalprabhup <ppprabhu2010@gmail.com>
2018-01-12 18:05:24 +0530

    comment Box Local service Modification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -24,7 +24,8 @@ import { ReviewComponent } from './review/review.component';
   ],
   imports: [
     BrowserModule,	  
-    AppRoutingModule
+    AppRoutingModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -6,10 +6,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review.component.css']
 })
 export class ReviewComponent implements OnInit {
+ 
+ public fieldArray: Array<any> = [];
+    public newAttribute: any = {};
 
-  constructor() { }
+    addFieldValue() {
+        this.fieldArray.push(this.newAttribute)
+        this.newAttribute = {};
+    }
+	constructor(){
+	 this.newAttribute= [{name:"Prabhu",email:"prabhu@utkrishta.com",comment:"asda asd asdad ad asd a asd"},
+	{name:"Perumal",email:"perumal@utkrishta.com",comment:"asda asd asdad ad asd a asd"},
+	{name:"Sudharsan",email:"ram@utkrishta.com",comment:"asda asd asdad ad asd a asd"}
+	] 
+	}
 
-  ngOnInit() {
+	  ngOnInit() {
+	   
   }
+ /*
+	
+	items:[{
+		this.name : string,
+		this.email : string,
+		this.comment : string
+	}];
+	
 
-}
+	  constructor() { 
+		this.items = [{name:"Prabhu",email:"prabhu@utkrishta.com",comment:"asda asd asdad ad asd a asd"},
+	{name:"Perumal",email:"perumal@utkrishta.com",comment:"asda asd asdad ad asd a asd"},
+	{name:"Sudharsan",email:"ram@utkrishta.com",comment:"asda asd asdad ad asd a asd"}
+	] 
+	 
+	  }
+	      
+  ngOnInit() {
+	   
+  }
+   addItems() {
+ 
+ this.items.push{{
+	 name:this.name;
+ email:this.email;
+ comment:this.comment;
+ }} ;
+ 
+ this.name="";
+ this.email="";
+ this.comment="";
+ 
+ }
+	*/
+  
+
+};
